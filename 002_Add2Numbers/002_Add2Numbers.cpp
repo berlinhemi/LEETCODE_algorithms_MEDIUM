3b852e64c9123f8395a49e9b4cdd98f64f2d3895
berlinhemi <dennn93@mail.ru>
2025-10-31 23:22:35 +0300

    feat: add project to 002_Add2Numbers task

---
+++ 002_Add2Numbers/002_Add2Numbers.cpp
@@ -0,0 +1,23 @@
+
+
+#include <iostream>
+
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ 
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
